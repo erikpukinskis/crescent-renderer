67501bdc645398e90f6a60b3e4b243ea23d0be29
Erik Pukinskis <erik@snowedin.net>
2019-02-19 00:40:20 -0800

    readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
+gradually over many incarnations    
+picking up momentum    
+like a spiral    
+culminating in a particular life    
+at the mercy of your mother's actions    
+protected only by your sack    
+unconscious existence    
+sack ruptured    
+unconscious existence ends    
+living being    
+pain and effort    
+passing    
+at the mercy of the elemental forces of the world    
+
 ![Lossy GIF of chickens](chickens-hi-def.gif)
---
+++ render-chicken.js
@@ -0,0 +1,18 @@
+
+// good breakdown of painterly effects to get simple rendering: https://www.youtube.com/watch?v=RPyrUI6s10w
+
+scrumBacklog.epics(
+  "gradually over many incarnations",
+  "picking up momentum",
+  "like a spiral",
+  "culminating in a particular life",
+  "at the mercy of your mother's actions",
+  "protected only by your sack",
+  "unconscious existence",
+  "sack ruptured",
+  "unconscious existence ends",
+  "living being",
+  "pain and effort",
+  "passing",
+  "at the mercy of the elemental forces of the world")
+
